f42fa95fd31f20548ba5b6eded0994f98c95f420
nelishah <97979211+nelishah@users.noreply.github.com>
2023-11-07 11:25:16 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const { engine } = require('express-handlebars');
 const session = require('express-session');
 const { generate } = require('randomstring');
 const { requireUser } = require('./utils.js');
-const { readFile, writeFile } = require('node:fs');
 
 const users = require('./user.json');
 
@@ -47,91 +46,11 @@ app.get('/login', (req, res) => {
 });
 
 app.get('/banking', requireUser, (req, res) => {
-  const { username, account, limitFund } = req.session;
-  if (account) {
-    return res.render('home', {
-      title: 'Banking',
-      bankPage: true,
-      username,
-      gotNewAccount: true,
-      accountNo: account.accountNo,
-      accountType: account.accountType,
-    });
-  }
-
-  if (limitFund) {
-    return res.render('home', {
-      title: 'Banking',
-      bankPage: true,
-      username,
-      gotError: true,
-      errorMessage: 'Insufficient Funds',
-    });
-  }
-
+  const { username } = req.session;
   return res.render('home', {
     title: 'Banking',
     bankPage: true,
     username,
-  });
-});
-
-app.get('/balance/:account', requireUser, (req, res) => {
-  const { account } = req.params;
-
-  // ReadFile function is used to read data from file
-  // asynchronously
-
-  // WriteFile function is used to write data to a file
-  // asynchronously
-
-  readFile('./accounts.json', 'utf-8', (err, data) => {
-    if (err) return res.sendStatus(500);
-
-    // The data received from readFile has to be converted
-    // into a JavaScript object for further modification
-    const accountData = JSON.parse(data);
-
-    const selectedAccount = accountData[account];
-
-    if (selectedAccount) {
-      const { accountType, accountBalance } = selectedAccount;
-
-      return res.render('home', {
-        title: 'Balance',
-        balancePage: true,
-        accountNo: account,
-        accountType,
-        accountBalance,
-      });
-    }
-  });
-});
-
-// The :account is a params object which can be used
-// to pass data around in the URL
-app.get('/deposit/:account', requireUser, (req, res) => {
-  const { account } = req.params;
-  return res.render('home', {
-    title: 'Deposit',
-    depositPage: true,
-    accountNo: account,
-  });
-});
-
-app.get('/account', requireUser, (req, res) => {
-  return res.render('home', {
-    title: 'Open Account',
-    accountPage: true,
-  });
-});
-
-app.get('/withdrawal/:account', requireUser, (req, res) => {
-  const { account } = req.params;
-  return res.render('home', {
-    title: 'Withdrawal',
-    withdrawalPage: true,
-    accountNo: account,
   });
 });
 
@@ -191,132 +110,6 @@ app.post('/logout', (req, res) => {
       }
     });
   }
-});
-
-app.post('/banking', (req, res) => {
-  const { option, account } = req.body;
-
-  readFile('./accounts.json', 'utf-8', (err, data) => {
-    if (err) return res.sendStatus(500);
-
-    const accountData = JSON.parse(data);
-    const isValidAccount = accountData[account];
-
-    if (option !== 'account' && !isValidAccount) {
-      return res.render('home', {
-        title: 'Banking',
-        bankPage: true,
-        gotError: true,
-        errorMessage:
-          'This account does not exist. Enter a valid account number',
-      });
-    }
-
-    // Switch statements are worth it if
-    // there are a lot of cases to be compared for
-    switch (option) {
-      case 'balance':
-        return res.redirect(`/balance/${account}`);
-      case 'deposit':
-        return res.redirect(`/deposit/${account}`);
-      case 'account':
-        return res.redirect('/account');
-      case 'withdrawal':
-        return res.redirect(`/withdrawal/${account}`);
-    }
-  });
-});
-
-app.post('/deposit', (req, res) => {
-  const { depositAmount, account } = req.body;
-
-  readFile('./accounts.json', 'utf-8', (err, data) => {
-    if (err) return res.statusCode(500);
-
-    const accountData = JSON.parse(data);
-    const { accountBalance } = accountData[account];
-    const newAccountBalance = accountBalance + Number(depositAmount);
-
-    accountData[account].accountBalance = newAccountBalance;
-
-    writeFile(
-      './accounts.json',
-      // This code converts a JavaScript object into
-      // a string with 2 tab spaces
-      JSON.stringify(accountData, null, 2),
-      (err) => {
-        if (err) return res.sendStatus(code);
-      }
-    );
-    return res.redirect('/banking');
-  });
-});
-
-app.post('/account', (req, res) => {
-  const { option } = req.body;
-  readFile('./accounts.json', 'utf-8', (err, data) => {
-    if (err) {
-      return res.sendStatus(500);
-    }
-
-    const accountData = JSON.parse(data);
-    const { lastID } = accountData;
-    const newID = Number(lastID) + 1;
-
-    // This code will add 7 or less 0 before the newIDStr
-    const newIDStr = String(newID).padStart(7, '0');
-
-    accountData[newIDStr] = {
-      accountType: option,
-      accountBalance: 0,
-    };
-
-    accountData.lastID = newIDStr;
-
-    writeFile(
-      './accounts.json',
-      JSON.stringify(accountData, null, 2),
-      (err) => {
-        if (err) {
-          return res.sendStatus(500);
-        }
-      }
-    );
-    req.session.account = {
-      accountType: option,
-      accountNo: newIDStr,
-    };
-    return res.redirect('/banking');
-  });
-});
-
-app.post('/withdrawal', (req, res) => {
-  const { withdrawalAmount, account } = req.body;
-  let isLimited = true;
-  readFile('./accounts.json', 'utf-8', (err, data) => {
-    if (err) return res.statusCode(500);
-
-    const accountData = JSON.parse(data);
-    const { accountBalance } = accountData[account];
-    const newAccountBalance = accountBalance - Number(withdrawalAmount);
-
-    if (newAccountBalance > 0) {
-      isLimited = false;
-      accountData[account].accountBalance = newAccountBalance;
-      writeFile(
-        './accounts.json',
-        JSON.stringify(accountData, null, 2),
-        (err) => {
-          if (err) return res.sendStatus(500);
-        }
-      );
-    }
-    req.session.limitFund = isLimited;
-    return res.redirect('/banking');
-  });
-});
-app.post('/cancel', (req, res) => {
-  return res.redirect('/banking');
 });
 
 app.listen(port, async () => {
